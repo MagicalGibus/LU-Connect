4247b1ecbed165b895b5bd57ebe941561d71f403
Joseph Seamark <j.seamark@lancaster.ac.uk>
2025-03-21 07:37:11 +0100

    Changed the main class of the server so that the IP and port number can be changed

--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -2,10 +2,31 @@ package Server;
 
 public class Main {
 
-    public static void main(String [] args) {
-        String host = "localhost";
-        int port = 1060;
+    public static void main(String[] args) {
+        String host = "localhost"; // Default
+        int port = 1060; // Default
 
+        // Parse command-line arguments
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-h") || args[i].equals("--host")) {
+                if (i + 1 < args.length) {
+                    host = args[i + 1];
+                    i++; 
+                }
+            } else if (args[i].equals("-p") || args[i].equals("--port")) {
+                if (i + 1 < args.length) {
+                    try {
+                        port = Integer.parseInt(args[i + 1]);
+                        i++; 
+                    } catch (NumberFormatException e) {
+                        System.err.println("Invalid port number");
+                        System.exit(1);
+                    }
+                }
+            }
+        }
+
+        System.out.println("Starting server on " + host + ":" + port);
         Server server = new Server(host, port);
         server.start();
     }
